2ca29c6450ec55d04d30a987c47f52a88271ff77
baghel-shivam <shivam@dreambox.cloud>
2024-02-11 14:02:52 +0530

    APi urls changes

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # path('', views.home, name='home'),
     # path('roomDetails/<int:id>', views.roomDetails, name='roomDetails'),
     
-    path('api/home',views.RoomSearchAPIView.as_view(), name='api_home'),
+    path('api/room_search',views.RoomSearchAPIView.as_view(), name='room_search'),
     path('api/room_details/<int:id>/',views.RoomDetailsAPIView.as_view(), name='api_room_details'),
     path('api/booking_request',views.BookingRequestApi.as_view(), name='api_booking_request'),
 
