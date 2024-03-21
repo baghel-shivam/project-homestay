675066b8b9cc1edd70c251ddcdddac913aaa0f6f
baghel-shivam <shivam@dreambox.cloud>
2024-03-21 21:28:56 +0530

    delete unencessory things

--- core/urls.py
+++ core/urls.py
@@ -9,9 +9,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin_you_can_handle_it/', admin.site.urls),          # Django admin route
-    path("", include("apps.authentication.urls")), # Auth routes - login / register
-    path("", include("apps.home.urls")),          # UI Kits Html files
+    path('admin_you_can_handle_it/', admin.site.urls),
     path("", include("mainapp.urls")) 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -19,44 +19,6 @@ from django.urls import reverse
 from django.views.generic.edit import FormView
 
 
-'''
-
-@login_required(login_url="/login/")
-def home(request):
-    if request.method == 'POST':
-        location = request.POST.get('location','')
-        checkin_date = request.POST.get('checkin_date','')
-        checkout_date = request.POST.get('checkout_date','')
-        available_rooms = Room.objects.filter(Q(parent_address__state__iexact = location)|Q(parent_address__city__iexact = location)| Q(parent_address__area__iexact = location), is_checked_in=False)
-        
-        context = {
-            'available_rooms':available_rooms,
-            'location':location,
-            'checkin_date':checkin_date,
-            'checkout_date':checkout_date,
-        }
-        template = 'home/room_search_results.html'
-        print(available_rooms[0].front_img.url)
-        return render(request,template, context)
-    template = 'home/mainHome.html'
-    return render(request,template)
-
-
-@login_required(login_url="/login/")
-def roomDetails(request,id):
-    selectedRoom = Room.objects.get(id=id)
-    allImages = RoomImages.objects.filter(parent_room=selectedRoom)
-    all_comments =  selectedRoom.booking_details.all().values('comment','rating','customer_name')
-    template = 'room/roomDetails.html'
-    context = {
-        'selectedRoom':selectedRoom,
-        'allImages':allImages,
-        'all_comments':all_comments
-    }
-    return render(request,template, context)
-
-'''
-
 # ****************Api Work here ****************
 class RoomSearchAPIView(APIView):
     def post(self, request):
@@ -68,6 +30,7 @@ class RoomSearchAPIView(APIView):
         available_rooms = Room.objects.filter(
             (Q(parent_address__state__iexact=location) |
              Q(parent_address__city__iexact=location) |
+             Q(site_name__icontains=location) |
              Q(parent_address__pincode__iexact=location) |
              Q(parent_address__area__iexact=location)),
              ~(Q(cheked_in_date__gte=checkin_date) & Q(cheked_in_date__lte=checkout_date)),
